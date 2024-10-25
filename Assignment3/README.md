68135aaf6c9ebb69d30420d27aa3871f3f6e5e29
Al-aziz19 <st135793@student.spbu.ru>
2024-10-25 21:47:40 +0300

    new

---
+++ Assignment3/README.md
@@ -0,0 +1,11 @@
+# Assigment3
+## Author
+**Раимбердиев Азиз, группа 24.Б83-мм**
+## Contacts
+st135793@student.spbu.ru
+## Description
+Создание иерархии трансформеров.
+## Build
+make
+## Run
+./transformer
---
+++ Assignment3/decepticon.cpp
@@ -0,0 +1,17 @@
+#include "decepticon.h"
+#include <iostream>
+
+Decepticon::Decepticon() {
+    std::cout << "Decepticon created.\n";
+}
+
+
+Decepticon::~Decepticon() {
+    std::cout << "Decepticon destroyed.\n";
+}
+
+
+bool Decepticon::sabotage() {
+    std::cout << "Decepticon sabotage.\n";
+    return true;
+}
