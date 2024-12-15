3d7290d18f744a1e4d7547d7fe4c1b37b9d0988a
Al-aziz19 <st135793@student.spbu.ru>
2024-12-15 15:10:51 +0300

    new

--- Assignment3/maximal.cpp
+++
@@ -1,8 +0,0 @@
-//Raimberdiyev Aziz st135793@student.spbu.ru
-///Transformer Class Inheritance
-#include "maximal.h"
-#include <iostream>
-
-void Maximal::transformer() {
-    std::cout << name << " is transforming!" << std::endl;
-}
